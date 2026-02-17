92c742e77ce87e1434ee0ad99b50650c0f6cc743
recursive404 <67127594+recursive404@users.noreply.github.com>
2026-02-16 23:29:42 -0800

    ci: deploy to github pages (#14)
    Closes #7

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_PAGES === 'true' ? '/rain-connections/' : '/',
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
